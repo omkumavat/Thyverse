aaeaf58826cdf2c8701d8a678443216b155a1fd1
omkumavat <omkumavat2004@gmail.com>
2025-02-19 11:19:46 +0530

    Backend

---
+++ Backend/index.js
@@ -0,0 +1,26 @@
+import express from "express";
+import mongoose from "mongoose";
+import bodyParser from "body-parser";
+import dotenv from "dotenv";
+import cors from 'cors';
+
+const app = express();
+app.use(bodyParser.json({ limit: '100mb' })); // Adjust the limit as needed
+app.use(bodyParser.urlencoded({ limit: '100mb', extended: true }));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+dotenv.config();
+
+app.use(cors({
+    origin: '*',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'], // Allowing these methods
+    allowedHeaders: ['Content-Type'], // Allow these headers
+}));
+
+app.get('/', (req, res) => {
+    res.send('Hello World')
+})
+
+app.listen(4000, () => {
+    console.log("app is listening on port 4000");
+});
